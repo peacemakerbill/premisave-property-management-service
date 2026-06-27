2133760debab6a7406a364aa874e96474ea94945
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-27 22:11:16 +0300

    first commit

---
+++ src/test/java/com/premisave/property/PremisavePropertyManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premisave.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremisavePropertyManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
