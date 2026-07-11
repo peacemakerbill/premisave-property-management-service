e5c7a5d94198790c8cfcb9b53911c3a05984a257
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-07-11 19:28:54 +0300

    Add dotenv support to load .env variables
    - Added dotenv-java dependency
    - Created DotenvConfig class
    - Updated main application class to load .env at startup

--- src/main/java/com/premisave/property/PremisavePropertyManagementApplication.java
+++ src/main/java/com/premisave/property/PremisavePropertyManagementApplication.java
@@ -1,5 +1,6 @@
 package com.premisave.property;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +12,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PremisavePropertyManagementApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        System.out.println("Dotenv loaded with " + dotenv.entries().size() + " variables");
+
         SpringApplication.run(PremisavePropertyManagementApplication.class, args);
     }
 }
